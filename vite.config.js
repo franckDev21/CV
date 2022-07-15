a813f3eadb1f924e0a3546f3dd4f7a4daf8ff7ef
franckDev21 <tutodevphp@gmail.com>
2022-07-15 23:27:08 +0100

    https

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
         laravel([
             'resources/css/app.css',
