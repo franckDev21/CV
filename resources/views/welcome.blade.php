136bffce82dbab1a502b44d12e19d4e8c58c5457
franckDev21 <tutodevphp@gmail.com>
2022-07-15 23:31:21 +0100

    https 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <style>
+        <style> 
             body {
                 font-family: 'Nunito', sans-serif;
             }
@@ -17,7 +17,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
-        <h1 class="text-purple-700 font-bold text-4xl px-4 py-3 rounded-full bg-gray-100 max-w-lg text-center mx-auto mt-4">Download my CV </h1>
+        <h1 class="text-purple-700 font-bold text-4xl px-4 py-3 rounded-full bg-gray-100 max-w-lg text-center mx-auto mt-4">Download my CV  </h1>
           
         {{-- <x-torchlight-code language='php'>
             echo "Hello World!";
